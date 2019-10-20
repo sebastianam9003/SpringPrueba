ee4bd743363e0a446a5ef6f48fd0e6e64552e2de
sebastianam9003 <56412154+sebastianam9003@users.noreply.github.com>
2019-10-20 08:27:48 +0200

    Add files via upload

---
+++ Saludos.java
@@ -0,0 +1,11 @@
+package com.trifulcas.springprueba;
+
+
+public class Saludos {
+	public String Hola(){
+		return "Hola que tal.";
+	}
+	public String Adios(){
+		return "Adios, que te vaya bien...";
+	}
+}
